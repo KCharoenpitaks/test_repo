ca29169f34f8ca5ec6c0bf85b6b8e62340cd7d30
KCharoenpitaks <50170318+KCharoenpitaks@users.noreply.github.com>
2024-08-27 19:22:32 +0900

    Add files via upload

---
+++ run_file1.sh
@@ -0,0 +1,117 @@
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name Accident
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name Accident_2
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name AccidentTwoWays
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name BlockedIntersection
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name ConstructionObstacle
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name ConstructionObstacleTwoWays
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name ControlLoss
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name DynamicObjectCrossing_right_container
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name DynamicObjectCrossing_right_haybalelb
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name EnterActorFlow_fast
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name EnterActorFlow_slow
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name HardBreakRoute
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name HazardAtSideLane
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name HazardAtSideLaneTwoWays
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name HighwayCutIn
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name HighwayExit
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name InterurbanActorFlow_fast
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name InterurbanActorFlow_slow
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name InterurbanAdvancedActorFlow_fast
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name InterurbanAdvancedActorFlow_slow
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name InterurbanAdvancedActorFlow_slow_2
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name InvadingTurn_less_curve
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name InvadingTurn_much_curve
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name MergerIntoSlowTraffic
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name MergerIntoSlowTrafficV2
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name NonSignalizedJunctionLeftTurn_fast
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name NonSignalizedJunctionLeftTurn_slow
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name NonSignalizedJunctionRightTurn_fast
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name NonSignalizedJunctionRightTurn_slow
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name OppositeVehicleRunningRedLight
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name OppositeVehicleTakingPriority
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name ParkedObstacle
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name ParkedObstacleTwoWays
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name ParkingCrossingPedestrian
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name ParkingCutIn
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name ParkingExit
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name PedestrianCrossing
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name SignalizedJunctionLeftTurn_fast
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name SignalizedJunctionLeftTurn_slow
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name SignalizedJunctionRightTurn_fast
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name SignalizedJunctionRightTurn_slow
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name StaticCutIn
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name VehicleOpensDoorTwoWays
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name VehicleTurningRoute_left
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name VehicleTurningRoute_right
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name VehicleTurningRoutePedestrian
+sleep 2m
+python capture_sensor_data.py --folder ScenarioLogs --scenario_name YieldToEmergencyVehicle
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 0
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 1
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 2
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 4
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 5
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 6
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 7
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 9
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 10
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 13
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 20
+sleep 2m
+python capture_sensor_data.py --folder RouteLogs --scenario_name 29
